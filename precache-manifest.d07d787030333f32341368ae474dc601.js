25359810aa3bc4b48c62c0f6d4f1afe565fb5423
Tobi Obadiah <tobiobadiah67@gmail.com>
2022-02-27 08:54:35 +0100

    deploy

---
+++ precache-manifest.d07d787030333f32341368ae474dc601.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234fb2d7674daa14c59f7e0d1a0f4e05",
+    "url": "/Tobi Obadiah - Resume.pdf"
+  },
+  {
+    "revision": "efcaf0ef802601bd008a",
+    "url": "/css/app.8c5b0969.css"
+  },
+  {
+    "revision": "8c45728d3678617aadb43018ff447e42",
+    "url": "/img/20271043.8c45728d.png"
+  },
+  {
+    "revision": "604f59977af3c5ec7207d0a8477feffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72388b1700dc56fb3d53",
+    "url": "/js/about.ab9d00ea.js"
+  },
+  {
+    "revision": "efcaf0ef802601bd008a",
+    "url": "/js/app.2f79b692.js"
+  },
+  {
+    "revision": "6ecf6df83a92fdee167f",
+    "url": "/js/chunk-vendors.b1d19fc6.js"
+  },
+  {
+    "revision": "295793fc40702807049023c0ff1d76a6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
