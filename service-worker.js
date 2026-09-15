2bce33d42abc9d73f15a3c3f1f8e5b0704885cd3
Tobi Obadiah <tobiobadiah67@gmail.com>
2026-09-15 20:58:51 +0100

    add projects

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65f34c73392516f30049c1755a5dd55d.js"
+  "/precache-manifest.90269bbefb6d70dc8d641ae8c66f9378.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tobi"});
